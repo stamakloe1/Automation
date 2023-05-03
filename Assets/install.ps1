809d74f6b62dccca6726d31e8ae9beee4a2ef2d2
stamakloe1 <118505389+stamakloe1@users.noreply.github.com>
2023-05-03 11:55:13 -0700

    Add files via upload

---
+++ Assets/install.ps1
@@ -0,0 +1,14 @@
+﻿#Author: Samuel Tamakloe 
+#$user = $env:username 
+$DesktopPath = [Environment]::GetFolderPath("Desktop")
+$path = $DesktopPath + '\folder_organizer\Install\apps'
+$path2 = $DesktopPath + '\folder_organizer\Install'
+$install = 'setup.pyw'
+#Write-Host $path
+Start-Process -Wait -FilePath $path\python-3.10.4-amd64.exe -ArgumentList '/S','v','/qn' -PassThru
+
+#$py = $path2+"\"+$install
+#Write-Host $py
+
+Start-Process py $py
+#Start-Process -FilePath $path2 "C:\Program Files\Python\python.exe" -ArgumentList '"\setup.pyw" "abcd"';
